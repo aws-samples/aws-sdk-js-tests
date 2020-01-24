f49c3a723bc0434be9b8269dadab5c447894173b
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-24 14:37:19 -0800

    chore: added script on the browser side

---
+++ src/files/script.js
@@ -0,0 +1,1 @@
+document.getElementById("hello").innerHTML = "Hello, from JavaScript!";
--- src/server.js
+++ src/server.js
@@ -4,6 +4,13 @@ const path = require("path");
 
 http
   .createServer((req, res) => {
-    fs.createReadStream(path.join(__dirname, "files/index.html")).pipe(res);
+    if (req.url === "/") {
+      fs.createReadStream(path.join(__dirname, "files/index.html")).pipe(res);
+    } else {
+      // regular expression for filename requested
+      const re = /\/(\w+)*/;
+      const filename = req.url.replace(re, "$1");
+      fs.createReadStream(path.join(__dirname, `/files/${filename}`)).pipe(res);
+    }
   })
   .listen(3000);
