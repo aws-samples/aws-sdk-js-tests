67556959f32d1cb082b44a1279b30001b0022f2d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-24 14:53:34 -0800

    chore: print AWS.VERSION in browser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const AWS = require("aws-sdk");
+
 function component() {
   const element = document.createElement("div");
 
-  element.innerHTML = "Hello, world!";
+  element.innerHTML = `Hello from AWS.VERSION ${AWS.VERSION}!`;
 
   return element;
 }
