9809dfcb58ab42949bec976aeb9c056d30c039a8
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-27 18:45:20 -0800

    feat: run SDK v3 in browser (#2)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 const AWS = require("aws-sdk");
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+const {
+  fromCognitoIdentityPool
+} = require("@aws-sdk/credential-provider-cognito-identity");
+const { CognitoIdentityClient } = require("@aws-sdk/client-cognito-identity");
 const { REGION, IDENTITY_POOL_ID } = require("./config");
 
-async function component() {
+const getHTMLElement = (title, content) => {
   const element = document.createElement("div");
+  element.style.margin = "30px";
 
+  const titleDiv = document.createElement("div");
+  titleDiv.innerHTML = title;
+  const contentDiv = document.createElement("textarea");
+  contentDiv.rows = 20;
+  contentDiv.cols = 50;
+  contentDiv.innerHTML = content;
+
+  element.appendChild(titleDiv);
+  element.appendChild(contentDiv);
+
+  return element;
+};
+
+const componentV2 = async () => {
   // Initialize the Amazon Cognito credentials provider
   AWS.config.region = REGION;
   AWS.config.credentials = new AWS.CognitoIdentityCredentials({
@@ -11,11 +31,33 @@ async function component() {
   });
   const v2Client = new AWS.DynamoDB();
   const response = await v2Client.listTables().promise();
-  element.innerHTML = `Data returned by v2: ${JSON.stringify(response)}!`;
 
-  return element;
-}
+  return getHTMLElement(
+    "Data returned by v2:",
+    JSON.stringify(response, null, 2)
+  );
+};
+
+const componentV3 = async () => {
+  const v3Client = new DynamoDB({
+    region: REGION,
+    credentials: fromCognitoIdentityPool({
+      client: new CognitoIdentityClient({
+        region: REGION,
+        credentials: () => Promise.resolve({})
+      }),
+      identityPoolId: IDENTITY_POOL_ID
+    })
+  });
+  const response = await v3Client.listTables({});
+
+  return getHTMLElement(
+    "Data returned by v3:",
+    JSON.stringify(response, null, 2)
+  );
+};
 
 (async () => {
-  document.body.appendChild(await component());
+  document.body.appendChild(await componentV2());
+  document.body.appendChild(await componentV3());
 })();
