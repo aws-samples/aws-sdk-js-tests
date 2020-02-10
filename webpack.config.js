c485d942810cb87a3a93dac479e8fc885328ffcf
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-02-09 16:56:21 -0800

    chore: call webpack-dev-server under start:dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const path = require("path");
+
 module.exports = {
   mode: "development",
   entry: {
     main: "./src/browser/index.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    open: true
   },
   watch: true
 };
