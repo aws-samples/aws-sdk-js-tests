f6f13d2c6f218bd1a4678b04ae0c47dbd68cbec3
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-04-01 08:08:07 -0700

    chore: prettier --write

--- .github/ISSUE_TEMPLATE/bug_report.md
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -1,10 +1,9 @@
 ---
 name: Bug report
 about: Create a report to help us improve
-title: ''
-labels: ''
-assignees: ''
-
+title: ""
+labels: ""
+assignees: ""
 ---
 
 <!--
@@ -19,6 +18,7 @@ A clear and concise description of what the bug is.
 
 **To Reproduce**
 Steps to reproduce the behavior:
+
 1. Go to '...'
 2. Click on '....'
 3. Scroll down to '....'
--- src/reactnative/App.js
+++ src/reactnative/App.js
@@ -7,7 +7,7 @@ const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
     "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
+    "Shake or press menu button for dev menu",
 });
 
 export default class App extends Component {
@@ -22,7 +22,7 @@ export default class App extends Component {
 
     this.setState({
       v2Response: JSON.stringify(v2Response, null, 2),
-      v3Response: JSON.stringify(v3Response, null, 2)
+      v3Response: JSON.stringify(v3Response, null, 2),
     });
   }
 
@@ -47,15 +47,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: "#F5FCFF",
   },
   content: {
     fontSize: 20,
-    margin: 10
+    margin: 10,
   },
   instructions: {
     textAlign: "center",
     color: "#333333",
-    marginBottom: 5
-  }
+    marginBottom: 5,
+  },
 });
