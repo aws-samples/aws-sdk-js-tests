990b60ae86a434ebc84d95a6f051e1d10083f8cf
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2020-12-15 14:10:15 -0800

    chore: rename src to packages and delete deps

--- packages/react-native/App.js
+++ packages/react-native/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 
-import { getV2BrowserResponse, getV3BrowserResponse } from "../shared/utils";
+import { getV2BrowserResponse, getV3BrowserResponse } from "../utils/utils";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
