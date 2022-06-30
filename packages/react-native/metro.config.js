69398a810e4ceceb636bbbcd85f25427fedf164d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-06-30 16:46:13 -0400

    chore(react-native): bump app to 0.69.1

--- packages/react-native/metro.config.js
+++ packages/react-native/metro.config.js
@@ -4,31 +4,14 @@
  *
  * @format
  */
-const fs = require('fs');
-const path = require('path');
-
-const workspaces = fs.readdirSync(path.resolve(__dirname, '../'));
-const currentWorkspace = path.basename(__dirname);
 
 module.exports = {
-  projectRoot: __dirname,
-  resolver: {
-    extraNodeModules: new Proxy(
-      {},
-      {
-        get: (target, name) => path.join(__dirname, `node_modules/${name}`),
-      },
-    ),
-  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },
-  watchFolders: workspaces
-    .filter((f) => f !== currentWorkspace)
-    .map((f) => path.join(__dirname, '../', f)),
 };
