0bfd2ab965b83833feab484080f648d38b979d97
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-07-22 15:37:04 -0700

    chore(react-native): simplify code in App.js (#189)

--- packages/react-native/App.js
+++ packages/react-native/App.js
@@ -7,18 +7,9 @@
  */
 
 import React, {useState} from 'react';
-import {
-  Button,
-  LogBox,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-} from 'react-native';
+import {Button, StyleSheet, ScrollView, View, Text} from 'react-native';
 
-import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 // React Native polyfills required for AWS SDK for JavaScript.
 import 'react-native-get-random-values';
@@ -26,10 +17,7 @@ import 'react-native-url-polyfill/auto';
 
 import {getV2BrowserResponse, getV3BrowserResponse} from '@aws-sdk/test-utils';
 
-// Refs: https://github.com/facebook/metro/issues/287#issuecomment-738622439
-LogBox.ignoreLogs(['Require cycle: node_modules']);
-
-const App: () => React$Node = () => {
+const App: () => Node = () => {
   const [v2Response, setV2Response] = useState('');
   const [v3Response, setV3Response] = useState('');
 
@@ -52,80 +40,47 @@ const App: () => React$Node = () => {
   };
 
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>
-                AWS SDK for JavaScript (v2):
-              </Text>
-              <Button title="Call with v2" onPress={fetchV2Response} />
-              <ScrollView style={styles.scrollView}>
-                <Text style={styles.sectionDescription}>{v2Response}</Text>
-              </ScrollView>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>
-                AWS SDK for JavaScript (v3):
-              </Text>
-              <Button title="Call with v3" onPress={fetchV3Response} />
-              <ScrollView style={styles.scrollView}>
-                <Text style={styles.sectionDescription}>{v3Response}</Text>
-              </ScrollView>
-            </View>
-          </View>
+    <View style={styles.container}>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>AWS SDK for JavaScript (v2):</Text>
+        <Button title="Call with v2" onPress={fetchV2Response} />
+        <ScrollView style={styles.resultWindow}>
+          <Text style={styles.sectionDescription}>{v2Response}</Text>
         </ScrollView>
-      </SafeAreaView>
-    </>
+      </View>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>AWS SDK for JavaScript (v3):</Text>
+        <Button title="Call with v3" onPress={fetchV3Response} />
+        <ScrollView style={styles.resultWindow}>
+          <Text style={styles.sectionDescription}>{v3Response}</Text>
+        </ScrollView>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
+  container: {
+    flex: 1,
   },
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    borderWidth: 1,
+    flex: 2,
+    padding: 16,
   },
   sectionTitle: {
-    fontSize: 24,
+    fontSize: 18,
+    textAlign: 'center',
     fontWeight: '600',
     color: Colors.black,
   },
+  resultWindow: {
+    backgroundColor: Colors.lighter,
+  },
   sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
+    fontSize: 14,
     fontWeight: '400',
     color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
 
