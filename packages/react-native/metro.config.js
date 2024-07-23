655ffa8caa9e0fb8d6e0a9527f2b311f95645379
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-07-22 22:23:24 -0700

    chore: metro.config workaround
    Refs: https://github.com/facebook/react-native/issues/38282#issuecomment-1866091543

--- packages/react-native/metro.config.js
+++ packages/react-native/metro.config.js
@@ -1,11 +1,39 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const {
+  resolver: {sourceExts, assetExts},
+} = getDefaultConfig(__dirname);
+
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const path = require('path');
+
+const config = {
+  resolver: {
+    extraNodeModules: new Proxy(
+      {},
+      {
+        get: (target, name) => path.join(__dirname, `node_modules/${name}`),
+      },
+    ),
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  watchFolders: [path.resolve(__dirname, '..', '..')],
+};
+
+module.exports = mergeConfig(defaultConfig, config);
